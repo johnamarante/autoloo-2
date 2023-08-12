498a4cd3e67fb003909cd1a961b47dc7d9405ac6
John Amarante <jamarante@sparkfish.com>
2023-08-12 00:15:58 -0700

    refactor

--- autoloo/Assets/scripts/FriendpasteClient.cs
+++ autoloo/Assets/scripts/FriendpasteClient.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,83 +9,70 @@ namespace FriendpasteClient
 {
     public static class FriendpasteClient
     {
-        public const string baseURL = "https://www.friendpaste.com/";
+        public const string BaseUrl = "https://www.friendpaste.com/";
         private const string JsonContentType = "application/json";
+        private static readonly HttpClient HttpClient = CreateHttpClient();
 
-        public static async Task<string> PostDataAsync(string postURL, string title, string body)
+        public static async Task<string> PostDataAsync(string postUrl, string title, string body)
         {
             var postData = GenerateJsonData(title, body);
+            using var content = new StringContent(postData, Encoding.UTF8, JsonContentType);
 
-            using (var client = CreateHttpClient())
-            using (var content = new StringContent(postData, Encoding.UTF8, JsonContentType))
-            {
-                var response = await client.PostAsync(postURL, content);
-                return await response.Content.ReadAsStringAsync();
-            }
+            var response = await HttpClient.PostAsync(postUrl, content);
+            return await response.Content.ReadAsStringAsync();
         }
 
-        public static async Task PutDataAsyncWithTimeout(string postURL, string title, string body)
+        public static async Task PutDataAsyncWithTimeout(string putUrl, string title, string body)
         {
             var putData = GenerateJsonData(title, body);
+            using var content = new StringContent(putData, Encoding.UTF8, JsonContentType);
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5)); // Timeout of x seconds
 
-            using (var client = CreateHttpClient())
-            using (var content = new StringContent(putData, Encoding.UTF8, JsonContentType))
-            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5))) // Timeout of x seconds
+            try
             {
-                try
-                {
-                    Console.WriteLine($"PUT will be sent to {postURL}");
-                    await client.PutAsync(postURL, content, cts.Token);
-                }
-                catch (OperationCanceledException)
-                {
-                    Console.WriteLine($"PUT operation timed out after 5 second.");
-                }
+                Console.WriteLine($"PUT will be sent to {putUrl}");
+                await HttpClient.PutAsync(putUrl, content, cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"PUT operation timed out after 5 seconds.");
             }
         }
 
-        public static void PutDataFireAndForget(string postURL, string title, string body)
+        public static void PutDataFireAndForget(string putUrl, string title, string body)
         {
             var putData = GenerateJsonData(title, body);
+            using var content = new StringContent(putData, Encoding.UTF8, JsonContentType);
 
-            using (var client = CreateHttpClient())
-            using (var content = new StringContent(putData, Encoding.UTF8, JsonContentType))
-            {
-                // Fire and forget by not awaiting the task
-                _ = client.PutAsync(postURL, content);
-                Console.WriteLine($"PUT sent to {postURL}");
-            }
+            // Fire and forget by not awaiting the task
+            _ = HttpClient.PutAsync(putUrl, content);
+            Console.WriteLine($"PUT sent to {putUrl}");
         }
 
-
-        public static async Task<string> GetDataAsync(string postURL)
+        public static async Task<string> GetDataAsync(string getUrl)
         {
-            using (var client = CreateHttpClient())
-            {
-                var response = await client.GetAsync(postURL);
-                return await response.Content.ReadAsStringAsync();
-            }
+            var response = await HttpClient.GetAsync(getUrl);
+            return await response.Content.ReadAsStringAsync();
         }
 
         private static string GenerateJsonData(string title, string body)
         {
             var jsonData = $@"{{""title"": ""{title}"",""snippet"": ""{body}"",""language"": ""text""}}";
-            //breaking this into a variable for readability and easier debugging
             return jsonData;
         }
 
         public static string PrepareJSONStringForBodyArgument(string input)
         {
             // Remove line breaks and double quotes from the input string
-            string output = input.Replace("\n", "").Replace("\r", "").Replace("\"", "`");
-            return output;
+            return input.Replace("\n", "").Replace("\r", "").Replace("\"", "`");
         }
+
         public static string PrepareFriendPasteSnippetForCSharpJSONParse(string input)
         {
             // Replace backticks with double quotes for JSON serialization in C#
-            string output = input.Replace("\n", "").Replace("\r", "").Replace("`", "\"");
-            return output;
+            return input.Replace("\n", "").Replace("\r", "").Replace("`", "\"");
         }
+
         private static HttpClient CreateHttpClient()
         {
             var httpClient = new HttpClient();
--- autoloo/Assets/scripts/MainMenu.cs
+++ autoloo/Assets/scripts/MainMenu.cs
@@ -2,8 +2,6 @@ using Auth0.AuthenticationApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -11,117 +9,111 @@ public class MainMenu : MonoBehaviour
 {
     private static string menuMessage = "";
     public static AutolooUserInfo autolooUserInfo;
-    public string directoryUrl = "";
-    public string directoryData = "";
-    public string directoryTitle = "";
-    public string userId = "";
-    public string playerName = "";
-    public string playerData = "";
+    private string userId = "";
     private bool newUserSetup = false;
     private GUIStyle guiStyle;
+
+    [Header("Directory")]
+    public string directoryUrl = "";
+    private string directoryData = "";
+
+    [Header("Player")]
+    public string playerName = "";
+    private string playerData = "";
 
     private void Awake()
     {
         guiStyle = new GUIStyle() { normal = new GUIStyleState() { textColor = Color.black }, fontSize = 48 };
     }
 
-    // Start is called before the first frame update
-    async void Start()
-    { 
+    private async void Start()
+    {
         autolooUserInfo = FindObjectOfType<AutolooUserInfo>();
         userId = autolooUserInfo.UserInfo.UserId.Split("|")[1];
 
-        //Get the user directory data based on the first char of the userid from Auth0
         directoryUrl = AutolooUserInfoUtil.GetUserDirectoryURLBasedOnUserID(userId);
         directoryData = await FriendpasteClient.FriendpasteClient.GetDataAsync(directoryUrl);
 
-        //check if the user is already in the directoy
         var pasteURL = GetPasteIdByUserId(directoryData, userId);
-        //if the user is already present in the directory are, then GET the user data
-        if (pasteURL is not null)
+        if (!string.IsNullOrEmpty(pasteURL))
         {
             var playerData = await FriendpasteClient.FriendpasteClient.GetDataAsync(pasteURL);
-            // Deserialize the main JSON object
-            JObject mainObject = JObject.Parse(playerData);
-
-            // Get the "snippet" property as a JSON string
-            string snippetJsonString = (string)mainObject["snippet"];
-
-            // Deserialize the inner JSON object within the "snippet" property
-            JObject snippetObject = JObject.Parse( FriendpasteClient.FriendpasteClient.PrepareFriendPasteSnippetForCSharpJSONParse( snippetJsonString));
-
-            // Now you can access properties within the inner JSON object
-            autolooUserInfo.PlayerName = (string)snippetObject["playername"];
+            autolooUserInfo.PlayerName = ExtractPlayerNameFromData(playerData);
         }
-        else {
-            //otherwise, do new user setup, which is
-            //POST, and get the URL of that post and
-            //PUT the URL of the POST that was just made into the right directory, mapped to the userID
-            //make a POST the new user data and PUT to the directory
+        else
+        {
             playerName = autolooUserInfo.UserInfo.Email.Split("@")[0];
             newUserSetup = true;
         }
-        menuMessage = $"logged in as {(String.IsNullOrEmpty(autolooUserInfo.PlayerName) ? autolooUserInfo.UserInfo.Email : autolooUserInfo.PlayerName)}, id {userId}";
+
+        menuMessage = $"Logged in as {GetPlayerDisplayName()}, id {userId}";
     }
 
-    // Update is called once per frame
-    void Update()
+    private string GetPlayerDisplayName()
     {
-        
+        return String.IsNullOrEmpty(autolooUserInfo.PlayerName) ? autolooUserInfo.UserInfo.Email : autolooUserInfo.PlayerName;
     }
 
-    private async Task OnGUI()
+    private async void OnGUI()
     {
         GUI.Label(new Rect(10, Screen.height - 60, 30, 1000), menuMessage, guiStyle);
 
         if (newUserSetup)
         {
-            await ShowNewUserSetupDialouge();
+            await ShowNewUserSetupDialogue();
         }
     }
 
-    private async Task ShowNewUserSetupDialouge()
+    private async Task ShowNewUserSetupDialogue()
     {
-        // Calculate the center of the screen
         float centerX = Screen.width * 0.5f;
         float centerY = Screen.height * 0.5f;
 
-        // Calculate the dimensions for the input field and button
         float inputWidth = 500;
         float inputHeight = 60;
         float buttonWidth = 500;
         float buttonHeight = 60;
 
-        // Calculate positions for the input field and button
         float inputX = centerX - inputWidth * 0.5f;
         float inputY = centerY - inputHeight * 0.5f;
         float buttonX = centerX - buttonWidth * 0.5f;
         float buttonY = inputY + inputHeight + 20;
 
-        // Draw the input field
         playerName = GUI.TextField(new Rect(inputX, inputY, inputWidth, inputHeight), playerName);
 
-        // Draw the button
         if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Set Name"))
         {
-            JObject jsonObject = new JObject(
-                new JProperty("playername", playerName),
-                new JProperty("UnitDetails", new JArray())
-            );
-
-            string jsonString = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
-            var newPlayerPostResponse = await FriendpasteClient.FriendpasteClient.PostDataAsync(FriendpasteClient.FriendpasteClient.baseURL, userId, FriendpasteClient.FriendpasteClient.PrepareJSONStringForBodyArgument(jsonString));
-
-            var updatedJSON = AddUserIdAndPasteIdToDirectoryData(directoryData, userId, JObject.Parse(newPlayerPostResponse).Value<string>("url"));
-            char firstChar = char.ToLower(userId[0]);
-            await FriendpasteClient.FriendpasteClient.PutDataAsyncWithTimeout(directoryUrl, $"AutolooUserDirectory{firstChar}" , FriendpasteClient.FriendpasteClient.PrepareJSONStringForBodyArgument(updatedJSON));
-
-            Debug.Log($"Player name set to {playerName} and paste is at {newPlayerPostResponse}");
-            newUserSetup = false;
+            await SetupNewUser();
         }
     }
 
-    static string GetPasteIdByUserId(string jsonString, string userId)
+    private async Task SetupNewUser()
+    {
+        JObject jsonObject = new JObject(
+            new JProperty("playername", playerName),
+            new JProperty("UnitDetails", new JArray())
+        );
+
+        string jsonString = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+        var newPlayerPostResponse = await FriendpasteClient.FriendpasteClient.PostDataAsync(FriendpasteClient.FriendpasteClient.BaseUrl, userId, FriendpasteClient.FriendpasteClient.PrepareJSONStringForBodyArgument(jsonString));
+
+        var updatedJSON = AddUserIdAndPasteIdToDirectoryData(directoryData, userId, JObject.Parse(newPlayerPostResponse).Value<string>("url"));
+        char firstChar = char.ToLower(userId[0]);
+        await FriendpasteClient.FriendpasteClient.PutDataAsyncWithTimeout(directoryUrl, $"AutolooUserDirectory{firstChar}", FriendpasteClient.FriendpasteClient.PrepareJSONStringForBodyArgument(updatedJSON));
+
+        Debug.Log($"Player name set to {playerName} and paste is at {newPlayerPostResponse}");
+        newUserSetup = false;
+    }
+
+    private string ExtractPlayerNameFromData(string playerData)
+    {
+        JObject mainObject = JObject.Parse(playerData);
+        string snippetJsonString = (string)mainObject["snippet"];
+        JObject snippetObject = JObject.Parse(FriendpasteClient.FriendpasteClient.PrepareFriendPasteSnippetForCSharpJSONParse(snippetJsonString));
+        return (string)snippetObject["playername"];
+    }
+
+    private string GetPasteIdByUserId(string jsonString, string userId)
     {
         JObject outerObject = JObject.Parse(jsonString);
         JObject innerObject = JObject.Parse(FriendpasteClient.FriendpasteClient.PrepareFriendPasteSnippetForCSharpJSONParse(outerObject["snippet"].ToString()));
@@ -135,10 +127,10 @@ public class MainMenu : MonoBehaviour
                 return user["pasteid"].ToString();
             }
         }
-        return null; // Return null if userId is not found
+        return null;
     }
 
-    static string AddUserIdAndPasteIdToDirectoryData(string jsonString, string userId, string pasteId)
+    private string AddUserIdAndPasteIdToDirectoryData(string jsonString, string userId, string pasteId)
     {
         var newElement = new JObject
         {
@@ -147,8 +139,6 @@ public class MainMenu : MonoBehaviour
         };
         JObject anotherJsonObject = JObject.Parse(FriendpasteClient.FriendpasteClient.PrepareFriendPasteSnippetForCSharpJSONParse(JObject.Parse(jsonString)["snippet"].ToString()));
         anotherJsonObject["data"][0].AddBeforeSelf(newElement);
-        string serializedAnotherJson = JsonConvert.SerializeObject(anotherJsonObject, Formatting.Indented);
-        return serializedAnotherJson;
+        return JsonConvert.SerializeObject(anotherJsonObject, Formatting.Indented);
     }
-
 }
