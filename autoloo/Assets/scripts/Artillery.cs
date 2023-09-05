2b006e893a460c2bbca9880e123af2a5879b0e57
John Amarante <jamarante@sparkfish.com>
2023-09-05 14:24:01 -0700

    Artillery WIP

--- autoloo/Assets/scripts/Artillery.cs
+++ autoloo/Assets/scripts/Artillery.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +15,8 @@ public class Artillery : MonoBehaviour
         {
             range = 5;
         }
-        else {
+        else 
+        {
             range = 3;
         }
     }
@@ -23,5 +25,30 @@ public class Artillery : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Fire()
+    {
+        if (Math.Abs(unit.QueuePosition) == 1)
+        {
+            ShootGrape();
+        }
+        else if (Math.Abs(unit.QueuePosition) <= range)
+        {
+            ShootBall();
+        }
+    }
+
+    public void ShootBall()
+    {
+        //get the enemy
+        //the enemy is the guy where the queue position is -1 * (myqueueposition/math.abs(myqueueposition))
+        //do not do the damage until the "flying ball" collides with the enemy unit
+        //the flying ball needs to get there before the attack pahse goes off (need to delay next beat? yes)
+    }
+
+    public void ShootGrape()
+    {
+
     }
 }
